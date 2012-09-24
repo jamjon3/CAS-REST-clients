521b8e29acf4f7a081311456c8d51bcc7597e4ac
James Jones <jamjon3@gmail.com>
2012-09-24 13:06:35 +0000

    [CASREST-3] Modified this to create an separate config file

--- Grails/CASRestClient/scripts/_Install.groovy
+++ Grails/CASRestClient/scripts/_Install.groovy
@@ -10,29 +10,32 @@
 //
 // ant.mkdir(dir: "${basedir}/grails-app/CASRestServices")
 
-     
-target(main: "Creates artifacts for the CAS Rest Client plugin (USF)") {
-    def appDir = "${basedir}/grails-app"
-    def configFile = new File(appDir, 'conf/Config.groovy')
-    def appName = Ant.project.properties.'base.name'    
-    
-    if (configFile.exists()) {
-        configFile.withWriterAppend {
-            it.writeLine '\n// Added by the CAS Rest Client (USF) plugin:'        
-            it.writeLine "CASRestClient.cas.server = 'https://authtest.it.usf.edu'"
-            it.writeLine "CASRestClient.cas.ticketsPath = '/v1/tickets'"
-            it.writeLine "CASRestClient.cas.username = 'YourCASUserName'"    
-            it.writeLine "CASRestClient.cas.password = 'YourCASPassword'"                  
-        }
-    }
-    
-    ant.echo """
-    ********************************************************
-    * Your grails-app/conf/Config.groovy has been updated. *
-    *                                                      *
-    * Please verify that the values are correct.           *
-    ********************************************************
-    """    
-}
 
-setDefaultTarget(main)
+ant.copy(file: "${pluginBasedir}/src/samples/CASRestClientConfig.groovy",
+         todir: "${basedir}/grails-app/conf")
+
+//target(main: "Creates artifacts for the CAS Rest Client plugin (USF)") {
+//    def appDir = "${basedir}/grails-app"
+//    def configFile = new File(appDir, 'conf/Config.groovy')
+//    def appName = Ant.project.properties.'base.name'    
+//    
+//    if (configFile.exists()) {
+//        configFile.withWriterAppend {
+//            it.writeLine '\n// Added by the CAS Rest Client (USF) plugin:'        
+//            it.writeLine "CASRestClient.cas.server = 'https://authtest.it.usf.edu'"
+//            it.writeLine "CASRestClient.cas.ticketsPath = '/v1/tickets'"
+//            it.writeLine "CASRestClient.cas.username = 'YourCASUserName'"    
+//            it.writeLine "CASRestClient.cas.password = 'YourCASPassword'"                  
+//        }
+//    }
+//    
+//    ant.echo """
+//    ********************************************************
+//    * Your grails-app/conf/Config.groovy has been updated. *
+//    *                                                      *
+//    * Please verify that the values are correct.           *
+//    ********************************************************
+//    """    
+//}
+//
+//setDefaultTarget(main)
