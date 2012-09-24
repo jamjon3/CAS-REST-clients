9b59cadf9b15914271b654b8a08a140b9036321f
James Jones <jamjon3@gmail.com>
2012-09-24 13:07:10 +0000

    [CASREST-3] Modified this to create an separate config file

---
+++ Grails/CASRestClient/src/samples/CASRestClientConfig.groovy
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+CASRestClient {
+    cas {
+        server = "https://authtest.it.usf.edu"
+        ticketsPath = "/v1/tickets"        
+        username = "myusername"    
+        password = "mypassword"            
+        
+    }
+}
